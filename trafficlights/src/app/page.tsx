548c5774ec13ceecffeffe92f3dc1e836f58a9ec
shubhat33 <rashidshubhat41@gmail.com>
2024-09-11 12:31:16 +0530

    additional ui specs

--- trafficlights/src/app/page.tsx
+++ trafficlights/src/app/page.tsx
@@ -31,7 +31,18 @@ export default function Page() {
     })
 
     return (
-        <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-10 h-full justify-center items-center">
+            <div className="flex justify-center items-center h-[30%] w-full border p-5 rounded-xl ">
+                <h1 className="font-bold text-sans" 
+                style={{
+                    fontSize:active === 'yellow'? '40px' : '100px',
+                    color : active === 'red' ? 'red' : active == 'green'? 'green' : 'yellow'
+                }}>
+                    {active === 'red' &&  <p>STOP </p>}
+                    {active === 'yellow' &&  <p>GET READY</p>}
+                    {active === 'green' &&  <p>Go</p>}
+                </h1>
+            </div>
             <div className="flex bg-black gap-5 p-5 rounded-xl">
                 <Light color={active=='red'?'#C40C0C':''}/>
                 <Light color={active=='yellow'?'#FFC700':''}/>
